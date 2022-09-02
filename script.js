dd5d0a3cecd9283f1291f55c89de1af8d9e24016
akaEnvi <envimth@gmail.com>
2022-09-03 00:43:05 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".button");
+const body = document.querySelector("body");
+const buttonTxt = document.querySelector(".button-txt");
+const on = document.querySelector("on");
+const off = document.querySelector("off");
+
+button.addEventListener("click", (e) => {
+  if (document.title === "Good Night") {
+    document.title = "Good Morning";
+  } else {
+    document.title = "Good Night";
+  }
+  button.classList.toggle("button-dark");
+  body.classList.toggle("body-dark");
+});
